1fc685f29819df4a26ab3dee5dfefb565e187c61
TOBY PETERSON <tspeterson206@gmail.com>
2019-10-09 20:31:58 -0700

    pruned

--- mainScript.js
+++ mainScript.js
@@ -55,23 +55,3 @@ $('#prev').click(function() {
 $('#next').click(function() {
   moveToSelected('next');
 });
-
-
-// MODAL SCRIPT
-
-    var modal = document.getElementById("myModal");
-    
-    var img = document.getElementById("myImg");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
-    }
-    
-    var span = document.getElementsByClassName("close")[0];
-    
-    span.onclick = function() { 
-      modal.style.display = "none";
-    }
